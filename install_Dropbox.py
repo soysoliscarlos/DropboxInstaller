c1279ceedea34c99ac3e9cbc4894cf91691449af
Carlos Solis Salazar <c.solis.s@gmail.com>
2016-06-19 20:20:53 -0400

    Upgrading files

--- install_Dropbox.py
+++ install_Dropbox.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 import sys
 import subprocess
 import os
 import socket
-#import ipaddress
-#import psutil
 
-Valid_OS_Version = ({'ubuntu': ['xenial']},
-                    {'debian': ['jessie']})
+ValidOSVersion = ({'ubuntu': ['hardy', 'intrepid', 'jaunty', 'karmic', 'lucid',
+                 'maverick', 'natty', 'oneiric', 'precise', 'quantal', 'raring',
+                 'saucy', 'trusty', 'utopic', 'vivid', 'wily', 'xenial']},
+                 {'debian': ['jessie', 'sid', 'squeeze', 'wheezy']})
 app_install = 'Dropbox'
-# Dropbox
 apt_key = 'apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E'
 source_list = '/etc/apt/sources.list.d/dropbox.list'
-deb_line = 'deb http://linux.dropbox.com/ubuntu/ xenial main'
+deb_line = 'deb http://linux.dropbox.com/%s/ %s main'
 principal_package = 'dropbox'
 packages = ()
 lock_file = '/var/run/install_dropbox.lock'
 
 
 def check_root():
+    """Checking if the user has root privileges. If is True return True"""
     if os.getuid() == 0:
         print("User has root privileges...\n")
         return True  # Return if user is root
@@ -49,10 +49,10 @@ def is_connected():
 
 
 def lock_process(_lock_file):
-    import pip
     try:
         import psutil
     except ImportError:
+        import pip
         pip.main(['install', 'psutil'])
         import psutil  # lint:ok
     if os.path.isfile(_lock_file):
@@ -65,10 +65,14 @@ def lock_process(_lock_file):
             print((('%s\n') % (_lock_file)))
             sys.exit(0)
             return True
+        else:
+            with open(_lock_file, "w") as lf:
+                lf.write(str(os.getpid()) + '\n')
+            return False
     elif not os.path.isfile(_lock_file):
         with open(_lock_file, "a") as lf:
             lf.write(str(os.getpid()) + '\n')
-            return False
+        return False
 
 
 def question(_Q, lock_file):
@@ -94,37 +98,39 @@ def del_file(_file):
     return
 
 
-def valid_value(_ValidTuple, _valuedict, _valuelist):
-    for _dict in _ValidTuple:
-        for key, value in _dict.items():
-            if key == _valuedict:
+def validingOSVersion(_ValidOsVersion, _MyOS, _OSVersion):
+    for _dict in _ValidOsVersion:
+        for key, value in list(_dict.items()):
+            if key == _MyOS:
                 for v in value:
-                    if v == _valuelist:
+                    if v == _OSVersion:
                         return True
-        return False
+                    else:
+                        print(('"%s" is not a supported OS Version for this script...\n'
+                             % (_OSVersion)))
+            else:
+                print(('"%s" is not a supported OS for this script...\n' %
+                     (_MyOS)))
+            return False
 
 
 class Linux_Cmd():
 
     def __init__(self, _MyOS, _OSName):
-        #self._packages = _packages
         _sudo = ''
         _MyOS = _MyOS.lower()
         if _MyOS == 'ubuntu':
             _sudo = 'sudo'
-        elif _MyOS == 'debian':
-            _sudo = ''
         self._sudo = _sudo
         self._MyOS = _MyOS
 
     def command(self, _cmd, stdout=True):
         _cmd = _cmd.split()
-        _cmd.insert(0, self._sudo)
+        if self._MyOS == 'ubuntu':
+            _cmd.insert(0, self._sudo)
         if not stdout:
-            #print('command self.stdout')
             subprocess.check_call(_cmd)
         else:
-            #print('command else ')
             subprocess.check_call((_cmd), stdout=subprocess.DEVNULL,
             stderr=subprocess.STDOUT)
 
@@ -140,8 +146,8 @@ class Linux_Cmd():
         if not self.check_pgk(_package):
             print(('Installing %s' % (_package)))
             if self._MyOS == 'ubuntu' or self._MyOS == 'debian':
-                self.command('apt install -y %s' % (_package))
-                print('OK...\n')
+                self.command('apt-get install -y %s' % (_package))
+            print('OK...\n')
         else:
             print(('%s is already install' % (_package)))
 
@@ -149,40 +155,38 @@ class Linux_Cmd():
         if type(_packages) is list or type(_packages) is tuple:
             for _package in _packages:
                 self.install_cmd(_package)
-            print('')
         else:
             self.install_cmd(_packages)
 
 
-def install_app():
+def install_app(MyOS, OSName):
     Q = 'Do you want install %s?' % (app_install)
     install = Linux_Cmd(MyOS, OSName)
     if not install.check_pgk(principal_package):
         if question(Q, lock_file):
             install.command(apt_key)
             if MyOS == 'ubuntu' or MyOS == 'debian':
-                if not os.path.isfile(source_list):
-                    with open(source_list, "a") as applist:
-                        applist.write(deb_line)
-                install.command('apt update')
+                with open(source_list, "w") as applist:
+                    applist.write(deb_line % (MyOS, OSName))
+                install.command('apt-get update')
                 install.install_cmd(principal_package)
                 if packages:
                     install.multi_install_cmd(packages)
     else:
-        print('%s is already install... \n' % (principal_package))
+        print(('%s is already install... \n' % (principal_package)))
         exit(0)
 
 
 if __name__ == '__main__':
     try:
         if sys.argv[1] and sys.argv[2]:
-            MyOS = sys.argv[1]
-            OSName = sys.argv[2]
+            MyOS = sys.argv[1].lower()
+            OSName = sys.argv[2].lower()
             if check_root():
                 if not lock_process(lock_file):
                     if is_connected():
-                        if valid_value(Valid_OS_Version, MyOS, OSName):
-                            install_app()
+                        if validingOSVersion(ValidOSVersion, MyOS, OSName):
+                            install_app(MyOS, OSName)
     except KeyboardInterrupt:
         print('\nExit by the user by pressing "Ctrl + c"...\n')
         del_file(lock_file)
